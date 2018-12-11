a3f01b1ffd730f20d83752fc857a98e737f0e447
Fairuzj <45782525+Fairuzj@users.noreply.github.com>
2018-12-11 17:05:54 +0700

    master cobaan1

---
+++ Latihan1.java
@@ -0,0 +1,9 @@
+public class Latihan1
+{
+	public static void main(String [] x)
+	{
+		String kelas = "Kelas XI RPL ";
+		int jumlahsiswa = 35;
+		System.out.println(kelas + "memiliki jumlah siswa sebanyak " + jumlahsiswa); 
+	}
+}
